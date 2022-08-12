d44c37bd6a0778c47bf7a868f67724e30653f897
Alexander Pototsky <silencemyalise@gmail.com>
2022-08-12 23:38:33 +0600

    add pre model + mapping + frimework

---
+++ WhoisObserver/Services/Model/ClientModel/WhoisRuModel.cs
@@ -0,0 +1,44 @@
+﻿namespace WhoisObserver.Services.Model.ClientModel
+{
+    public class WhoisRuModel
+    {
+        public string adminc { get; set; }
+        public string address { get; set; }
+        public string inetnum { get; set; }
+        public string counts { get; set; }
+        public string comment { get; set; }
+        public string created { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
+        public string netrange { get; set; }
+        public string descr { get; set; }
+        public string status { get; set; }
+        public string stateprov { get; set; }
+        public string cidr { get; set; }
+        public string netname { get; set; }
+        public string nethandle { get; set; }
+        public string mntby { get; set; }
+        public string parent { get; set; }
+        public string nettype { get; set; }
+        public string regdate { get; set; }
+        public string updated { get; set; }
+        public string postalcode { get; set; }
+        public string org { get; set; }
+        public string orgname { get; set; }
+        public string orgid { get; set; }
+        public string orgtechhandle { get; set; }
+        public string orgtechname { get; set; }
+        public string orgtechphone { get; set; }
+        public string orgtechemail { get; set; }
+        public string orgtechref { get; set; }
+        public string orgabusehandle { get; set; }
+        public string orgabusename { get; set; }
+        public string orgabusephone { get; set; }
+        public string orgabuseemail { get; set; }
+        public string orgabuseref { get; set; }
+        public string origin { get; set; }
+        public string organization { get; set; }
+        public string lastmodified { get; set; }
+        public string techc { get; set; }
+    }
+}
--- WhoisObserver/Services/WhoisServersClients/WhoisRuClient.cs
+++ WhoisObserver/Services/WhoisServersClients/WhoisRuClient.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
+using HtmlAgilityPack;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Xml;
 using WhoisObserver.Services.Model;
 using WhoisObserver.Services.WhoisServersClients.Interfaces;
 
@@ -19,7 +22,20 @@ namespace WhoisObserver.Services.WhoisServersClients
         public Task<string> ResponceJson(string host)
         {
             string url = request + host;
-            throw new NotImplementedException();
+            string HtmlAgilityPack()
+            {
+                //var url = "https://whois.ru/8.8.8.8";
+                //var url = "https://whois.ru/185.140.148.159";
+                //var url = "https://whois.ru/ya.ru";
+                HtmlWeb web = new HtmlWeb();
+                HtmlDocument doc = web.Load(url);
+
+                HtmlNode collection = doc.GetElementbyId("id");
+                HtmlNodeCollection span = doc.DocumentNode.SelectNodes("//pre[contains(@class, 'raw-domain-info-pre')]");
+
+                return span.First().InnerText;
+            }
+
         }
 
         public Task<WhoisResponseModel> ResponceObject(string host)
